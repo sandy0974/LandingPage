717ddaa5f96c93d0d9e23c1420aed03fe9796534
sandy0974 <136100432+sandy0974@users.noreply.github.com>
2026-09-20 17:59:04 +0700

    Delete generate_password.php

--- generate_password.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Buka sekali di browser, salin hasil hash ke SQL, lalu HAPUS file ini.
-$password = 'GANTI_PASSWORD_ANDA';
-echo '<pre>'.htmlspecialchars(password_hash($password, PASSWORD_DEFAULT)).'</pre>';
